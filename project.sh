ae3a3203f20894ada21c91e23be0d3379527f41c
automation <automation@modelingvalue.com>
2020-11-24 10:48:15 +0000

    automatic reformat by actions

--- project.sh
+++ project.sh
@@ -23,7 +23,7 @@
 #       s = sources
 #       t = test
 #===================================================================
-version='1.1.2'
+version='1.1.3'
 artifacts=(
   "org.modelingvalue   mvgjson                 $version    jar jds"
 )
