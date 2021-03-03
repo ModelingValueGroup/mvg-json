27a7c602d7712bc821cada790278894076f15a6e
Tom Brus <tombrus@gmail.com>
2021-03-03 13:19:45 +0100

    additional changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,13 +20,6 @@ plugins {
     `maven-publish`
     id("org.modelingvalue.gradle.mvgplugin") version "0.4.21"
 }
-dependencies {
-    
-    
-    //testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
-    //testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-
-}
 publishing {
     publications {
         create<MavenPublication>("mvgjson") {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,8 +15,6 @@
 
 rootProject.name = "mvgjson"
 
-include("mvgJson")
-
 plugins {
     id("com.gradle.enterprise") version ("3.5")
 }
