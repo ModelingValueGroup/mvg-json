3027bb25f9f4742d5e6136f4c41301dfb09a50b5
Tom Brus <tombrus@gmail.com>
2021-07-28 15:34:26 +0200

    prepare for release

--- project.sh
+++ project.sh
@@ -23,7 +23,7 @@
 #       s = sources
 #       t = test
 #===================================================================
-version='1.1.9'
+version='1.6.0'
 artifacts=(
   "org.modelingvalue   mvgjson                 $version    jar jds"
 )
