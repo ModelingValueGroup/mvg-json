1f0819a96ed1e3a4baef59d8729343b1770c4cfa
Tom Brus <tombrus@gmail.com>
2022-11-04 23:20:25 +0100

    prepare for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ repositories {
     maven("https://maven.pkg.github.com/ModelingValueGroup/sync-proxy")
 }
 dependencies {
-    testImplementation("org.modelingvalue:sync-proxy:2.1.0-BRANCHED")
+    testImplementation("org.modelingvalue:sync-proxy:2.1.1-BRANCHED")
 }
 publishing {
     publications {
