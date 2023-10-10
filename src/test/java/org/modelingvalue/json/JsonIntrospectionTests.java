a5fb56c2e8a747cad43af70cac9d0a43c026038d
Tom Brus <tombrus@gmail.com>
2023-10-10 16:06:41 +0200

    final fields can be set with introspection

--- src/test/java/org/modelingvalue/json/JsonIntrospectionTests.java
+++ src/test/java/org/modelingvalue/json/JsonIntrospectionTests.java
@@ -18,11 +18,14 @@ package org.modelingvalue.json;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.RepeatedTest;
 
+import java.util.Objects;
+
+import static org.modelingvalue.json.Json.fromJson;
 import static org.modelingvalue.json.Json.toJson;
 
 public class JsonIntrospectionTests {
 
-    @SuppressWarnings({"unused", "FieldMayBeFinal"})
+    @SuppressWarnings({"unused", "FieldMayBeFinal", "ConstantValue"})
     public static class ProbeA {
         int aap01 = 1;
         private                int aap02 = 2;
@@ -45,9 +48,26 @@ public class JsonIntrospectionTests {
         final static public    int aap16 = 15;
 
         int aap99 = 99;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            ProbeA probeA = (ProbeA) o;
+            return aap01 == probeA.aap01 && aap02 == probeA.aap02 && aap03 == probeA.aap03 && aap04 == probeA.aap04 && aap09 == probeA.aap09 && aap10 == probeA.aap10 && aap11 == probeA.aap11 && aap12 == probeA.aap12 && aap99 == probeA.aap99;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(aap01, aap02, aap03, aap04, aap09, aap10, aap11, aap12, aap99);
+        }
     }
 
-    @SuppressWarnings({"unused", "FieldMayBeFinal"})
+    @SuppressWarnings({"unused", "FieldMayBeFinal", "ConstantValue"})
     public static class ProbeB extends ProbeA {
         int aap01 = 101;
         private                int aap02 = 102;
@@ -78,40 +98,61 @@ public class JsonIntrospectionTests {
         String getName() {
             return "xyzzy";
         }
+        void setName(String name){
+            // nop
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            ProbeB probeB = (ProbeB) o;
+            return aap01 == probeB.aap01 && aap02 == probeB.aap02 && aap03 == probeB.aap03 && aap04 == probeB.aap04 && aap09 == probeB.aap09 && aap10 == probeB.aap10 && aap11 == probeB.aap11 && aap12 == probeB.aap12 && aap_ignore == probeB.aap_ignore && Objects.equals(probea, probeB.probea);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(aap01, aap02, aap03, aap04, aap09, aap10, aap11, aap12, aap_ignore, probea);
+        }
     }
 
     @RepeatedTest(1)
     public void listsToJson() {
         Assertions.assertEquals("{"
-                                + "\"aap01\":101,"
-                                + "\"aap02\":102,"
-                                + "\"aap03\":103,"
-                                + "\"aap04\":104,"
-                                //aap05: static field not in json
-                                //aap06: static field not in json
-                                //aap07: static field not in json
-                                //aap08: static field not in json
-                                + "\"aap09\":109,"
-                                + "\"aap10\":1010,"
-                                + "\"aap11\":1011,"
-                                + "\"aap12\":1012,"
-                                //aap13: static field not in json
-                                //aap14: static field not in json
-                                //aap15: static field not in json
-                                //aap16: static field not in json
-                                + "\"aap99\":99,"
-                                + "\"name\":\"xyzzy\","
-                                + "\"probea\":{"
-                                + /**/"\"aap01\":1,"
-                                + /**/"\"aap02\":2,"
-                                + /**/"\"aap03\":3,"
-                                + /**/"\"aap04\":4,"
-                                + /**/"\"aap09\":9,"
-                                + /**/"\"aap10\":10,"
-                                + /**/"\"aap11\":11,"
-                                + /**/"\"aap12\":12,"
-                                + /**/"\"aap99\":99"
-                                + "}"
-                                + "}", toJson(new ProbeB()));
+                                        + "\"aap01\":101,"
+                                        + "\"aap02\":102,"
+                                        + "\"aap03\":103,"
+                                        + "\"aap04\":104,"
+                                        //aap05: static field not in json
+                                        //aap06: static field not in json
+                                        //aap07: static field not in json
+                                        //aap08: static field not in json
+                                        + "\"aap09\":109,"
+                                        + "\"aap10\":1010,"
+                                        + "\"aap11\":1011,"
+                                        + "\"aap12\":1012,"
+                                        //aap13: static field not in json
+                                        //aap14: static field not in json
+                                        //aap15: static field not in json
+                                        //aap16: static field not in json
+                                        + "\"aap99\":99,"
+                                        + "\"name\":\"xyzzy\","
+                                        + "\"probea\":{"
+                                        + /**/"\"aap01\":1,"
+                                        + /**/"\"aap02\":2,"
+                                        + /**/"\"aap03\":3,"
+                                        + /**/"\"aap04\":4,"
+                                        + /**/"\"aap09\":9,"
+                                        + /**/"\"aap10\":10,"
+                                        + /**/"\"aap11\":11,"
+                                        + /**/"\"aap12\":12,"
+                                        + /**/"\"aap99\":99"
+                                        + "}"
+                                        + "}", toJson(new ProbeB()));
+        Assertions.assertEquals(new ProbeB(), fromJson(ProbeB.class, toJson(new ProbeB())));
     }
 }
